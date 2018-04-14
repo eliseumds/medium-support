f82a52e2c23c607e441c522c153f73a0130afef9
Eliseu Monar <eliseumds@gmail.com>
2018-04-14 16:21:30 +1000

    (forms-at-scale) add reduxFormPatchesMiddleware.js

---
+++ forms-at-scale-2-years-of-redux-form/reduxFormPatchesMiddleware.js
@@ -0,0 +1,51 @@
+import { actionTypes } from 'redux-form';
+
+const state = {};
+
+// See https://github.com/erikras/redux-form/issues/2742#issuecomment-308840160
+function onRegisterField(next, action, store) {
+  if (__SERVER__) { // << injected by Webpack
+    return store.getState().form[action.meta.form];
+  }
+
+  return next(action);
+}
+
+// See https://github.com/erikras/redux-form/issues/3435#issuecomment-359371803
+function onInitialize(next, action) {
+  // Bypass reinitialization
+  if ('lastInitialValues' in action.meta) {
+    return next(action);
+  }
+
+  state[action.meta.form] = (state[action.meta.form] || 0) + 1;
+
+  return next(action);
+}
+
+// See https://github.com/erikras/redux-form/issues/3435#issuecomment-359371803
+function onDestroy(next, action) {
+  state[action.meta.form] = (state[action.meta.form] || 0) - 1;
+
+  if (state[action.meta.form] <= 0) {
+    return next(action);
+  }
+
+  // Drop the action
+  return false;
+}
+
+export default function reduxFormPatchesMiddleware(store) {
+  return next => action => {
+    switch (action.type) {
+      case actionTypes.REGISTER_FIELD:
+        return onRegisterField(next, action, store);
+      case actionTypes.DESTROY:
+        return onDestroy(next, action);
+      case actionTypes.INITIALIZE:
+        return onInitialize(next, action);
+      default:
+        return next(action);
+    }
+  };
+}
